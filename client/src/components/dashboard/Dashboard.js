95758a9b574db005fb934665514b54dc49d64975
nguyenkhajim <nguyenkhajim.job@gmail.com>
2020-02-20 21:45:08 +0700

    add dashboard component

---
+++ client/src/components/dashboard/Dashboard.js
@@ -0,0 +1,23 @@
+import React, { useEffect } from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+
+import { getCurrentProfile } from '../../actions/profile';
+
+const Dashboard = ({ auth, getCurrentProfile, profile }) => {
+  useEffect(() => {
+    getCurrentProfile();
+  }, []);
+  return <div>Dashboard</div>;
+};
+
+Dashboard.propTypes = {
+  auth: PropTypes.object.isRequired,
+  getCurrentProfile: PropTypes.func.isRequired,
+  profile: PropTypes.object.isRequired
+};
+const mapStateToProps = (state) => ({
+  auth: state.auth,
+  profile: state.profile
+});
+export default connect(mapStateToProps, { getCurrentProfile })(Dashboard);
